441423698b354b49644c2cbce21a7222a4af0ae3
AntonParutov <ant.parutov@gmail.com>
2023-01-22 10:48:58 +0300

    update slider 4

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Массив картинок с ключ\значениями
 const imagesWithParam = [
   {
     url: "./slider_images/image1.png",
@@ -26,16 +25,23 @@ const imagesWithParam = [
   },
 ];
 
-// Функция работы слайдера
-function sliderEngine(imagesWithParam) {
+function sliderEngine(imagesWithParam, options) {
   if (!imagesWithParam || !imagesWithParam.length) return;
 
+  options = options || {
+    dots: false
+  }
   const sliderWrapper = document.querySelector(".slider_wrapper");
   const sliderImages = sliderWrapper.querySelector(".projects__image");
   const sliderArrows = document.querySelector(".vector_elements");
+  let dotsWrapper = document.querySelector(".vector_elements_center");
 
   initImages();
   initArrows();
+
+  if (options.dots){
+    initDots();
+  }
 
   function initImages() {
     imagesWithParam.forEach((image, index) => {
@@ -51,9 +57,9 @@ function sliderEngine(imagesWithParam) {
     let lastIndex = imagesWithParam.length - 1;
     sliderArrows.querySelectorAll(".vector_elements_arrow").forEach((arrow) => {
       arrow.addEventListener("click", function () {
-        let curNumber = +sliderImages.querySelector("active").dataset.index;
+        let curNumber = +sliderImages.querySelector(".active").dataset.index;
         let nextNumber;
-        if (arrow.classList.containes("left")) {
+        if (arrow.classList.contains("left")) {
           nextNumber = curNumber === 0 ? lastIndex : curNumber - 1;
         } else {
           nextNumber = curNumber === lastIndex ? 0 : curNumber + 1;
@@ -65,10 +71,27 @@ function sliderEngine(imagesWithParam) {
 
   function moveSlider(num){
     sliderImages.querySelector('.active').classList.remove('active');
-    sliderImages.querySelector(`n${num}`).classList.add('active');
+    sliderImages.querySelector(`.n${num}`).classList.add('active');
+  }
+
+  function initDots(){
+    imagesWithParam.forEach((image, index) => {
+      let dot = document.createElement('div');
+      dot.className = `slider_dot n${index} ${index ? "" : "active"}`;
+      dot.dataset.index = index;
+      dot.addEventListener('click', function(){
+        moveSlider(this.dataset.index);
+        dotsWrapper.querySelector(".active").classList.remove("active");
+        this.classList.add("active");
+      })
+      dotsWrapper.appendChild(dot);
+    });
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  sliderEngine(imagesWithParam);
+  let sliderDots = {
+    dots: true
+  }
+  sliderEngine(imagesWithParam, sliderDots);
 });
